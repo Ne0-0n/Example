cd549732bce4135db64e3ba30cc31fbde48309fc
Ne0-0n <egorosha.kutyaev@gmail.com>
2026-09-09 22:44:50 +0300

    изменена структура проекта

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,10 +1,59 @@
 #include <iostream>
-int main(){
-	double a, h, s;
-	std::cout << "Input side and height: ";
-	std::cin >>  a >> h;
-	s = 0.5 * a * h;
-	std::cout << "Area =" << s << std::endl;
 
+class Triangle {
+	int _side;
+	int _h;
+public:
+	Triangle();
+	Triangle(int, int);
+	Triangle(const Triangle&);
+
+	int side();
+	int h();
+
+	void side(int);
+	void h(int);
+
+	double calculate_area();
+};
+
+Triangle::Triangle() {
+	_side = 1;
+	_h = 3;
+}
+Triangle::Triangle(int side, int h) {
+	_side = side;
+	_h = h;
+}
+Triangle::Triangle(const Triangle& other) {
+	_side = other._side;
+	_h = other._h;
+}
+
+int Triangle::side() {
+	return _side;
+}
+int Triangle::h() {
+	return _h;
+}
+
+void Triangle::side(int side) {
+	_side = side;
+}
+void Triangle::h(int h) {
+	_h = h;
+}
+
+double Triangle::calculate_area() {
+	return 0.5 * _side * _h;
+}
+
+
+
+int main(){
+	Triangle triangle;
+	double s;
+	s = triangle.calculate_area();
+	std::cout << s;
 	return 0;
 }
