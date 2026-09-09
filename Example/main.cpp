ae369ad81e83a76787b605040c5c25bc67ece52e
Ne0-0n <egorosha.kutyaev@gmail.com>
2026-09-09 22:55:33 +0300

    изменена структура проекта

--- Example/main.cpp
+++ Example/main.cpp
@@ -4,9 +4,13 @@
 
 
 int main() {
-	Triangle triangle;
+	
 	double s;
+	double a, h;
+	std::cout << "Input side and height: ";
+	std::cin >> a >> h;
+	Triangle triangle(a,h);
 	s = triangle.calculate_area();
-	std::cout << s;
+	std::cout << "Area= " << s;
 	return 0;
 }
